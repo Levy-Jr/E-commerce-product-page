6014d885c7031f691a607edcdfa179352563decf
Levy Gomes <levybusinessjr@gmail.com>
2023-02-02 14:25:49 -0400

    fixing accessibility issues

--- public/javascript/header.js
+++ public/javascript/header.js
@@ -7,10 +7,6 @@ const menuLinks = document.querySelector('[data-ul-list')
 const header = document.querySelector('[data-primary-header]')
 
 openMenuButton.addEventListener('click', () => {
-    menuLinks.hasAttribute('data-visible') ? 
-    openMenuButton.setAttribute('aria-expanded', false) : 
-    openMenuButton.setAttribute('aria-expanded', true)
-
     ulWrapper.toggleAttribute('data-visible')
     closeMenuButton.toggleAttribute('data-visible')
     menuLinks.toggleAttribute('data-visible')
